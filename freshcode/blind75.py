a376fcee78a7f88bf33cf9c1a312541cec0f6448
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-03-01 18:53:51 -0500

    Finished sliding window problems for blind75

--- freshcode/blind75.py
+++ freshcode/blind75.py
@@ -316,6 +316,52 @@ def lengthOfLongestSubstring(s):
 # print(lengthOfLongestSubstring("bbbbb")) # 1
 # print(lengthOfLongestSubstring("pwwkew")) # 3
 
+# 424 - longest Repeating Character Replacement - 
+def characterReplacement(s, k):
+  dic = {}
+  res = 0
+  l = 0
+  for r in range(len(s)):
+    dic[s[r]] = 1 + dic.get(s[r], 0) # increment or assaign the charcter in the dictionary.
+    while (r-l+1) - max(dic.values()) > k: # checks if the window length is greater than our max length.
+      dic[s[l]] -= 1
+      l += 1
+    res = max(res, r - l + 1)
+  return res
+# print(characterReplacement('ABAB', 2)) # 4
+# print(characterReplacement('AABABBA', 1)) # 4
+
+# 76 - Minimum Window SubString - 
+def minWindow(s, t):
+  if t == "": return ""
+  countT, window = {}, {}
+  for c in t:
+    countT[c] = 1 + countT.get(c, 0)
+  have, need = 0, len(countT)
+  res, resLen = [-1, -1], float("infinity")
+  l = 0
+  for r in range(len(s)):
+    c = s[r]
+    window[c] = 1 + window.get(c, 0)
+    if c in countT and window[c] == countT[c]:
+      have += 1
+    while have == need:
+      # update our result
+      if (r - l + 1) < resLen:
+        res = [l, r]
+        resLen = (r-l+1)
+      # pop from the left of our window
+      window[s[l]] -= 1
+      if s[l] in countT and window[s[l]] < countT[s[l]]:
+        have -= 1
+      l += 1
+  l, r = res
+  return s[l:r+1] if resLen != float("infinity") else ""
+# print(minWindow("ADOBECODEBANC", "ABC")) # "BANC"
+# print(minWindow("a", "a")) # "a"
+
+
+
 # ?Sliding Window =======================================
 
 
