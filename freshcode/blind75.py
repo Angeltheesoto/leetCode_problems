4a54211ce6f7a2aaf9ffc3fc5c0d50902f2fb628
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-03-02 17:22:30 -0500

    Finished working on binary search problems

--- freshcode/blind75.py
+++ freshcode/blind75.py
@@ -359,9 +359,6 @@ def minWindow(s, t):
   return s[l:r+1] if resLen != float("infinity") else ""
 # print(minWindow("ADOBECODEBANC", "ABC")) # "BANC"
 # print(minWindow("a", "a")) # "a"
-
-
-
 # ?Sliding Window =======================================
 
 
@@ -403,6 +400,55 @@ def isValid(s):
 # ?Stack =======================================
 
 # ?Binary Search =======================================
+# 153 - Find Minimum in Rotated Sorted Array - Binary Search
+def findMin(nums):
+  res = nums[0]
+  low = 0
+  high = len(nums)-1
+  while low <= high:
+      if nums[low] < nums[high]:
+        res = min(res, nums[low])
+        break
+      mid = (low + high) // 2
+      res = min(res, nums[mid])
+      if nums[mid] >= nums[low]:
+        low = mid + 1
+      else:
+        high = mid - 1
+  return res
+# print(findMin([3,4,5,1,2])) # 1
+# print(findMin([4,5,6,7,0,1,2])) # 0
+# print(findMin([11,13,15,17])) # 11
+
+# 33 - Search in Rotated Sorted Array - Binary Search
+def search(nums, target):
+  low = 0
+  high = len(nums)-1
+  while low <= high:
+    mid = (low + high) // 2
+    if target == nums[mid]:
+      return mid
+    # left sorted portion
+    if nums[mid] >= nums[low]:
+      if target > nums[mid] or target < nums[low]:
+        low = mid + 1
+      else:
+        high = mid - 1
+    # right sorted portion
+    else:
+      if target < nums[mid] or target > nums[high]:
+        high = mid - 1
+      else:
+        low = mid + 1
+  return - 1
+
+print(search([4,5,6,7,0,1,2], 0)) # 4
+print(search([4,5,6,7,0,1,2], 3)) # -1
+print(search([1], 0)) # -1
+print(search([1, 3, 5], 3)) # 1
+print(search([4,5,6,7,0,1,2], 5)) # 1
+
+
 # ?Binary Search =======================================
 
 # ?Linked List =======================================
